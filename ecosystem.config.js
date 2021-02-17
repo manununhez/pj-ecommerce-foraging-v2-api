20d6ecfdc934a071699285ef8d08150ca6d3ad93
manununhez <manuel.nunhez90@gmail.com>
2021-02-17 18:01:46 +0000

    Bug Fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps : [{
   	name: 'pj-ecomm',
-        script: './server-pj-ecomm.js',
+        script: 'server-pj-ecomm.js',
         watch: true,
         env_production: {
-            "PORT": 5002,
             "NODE_ENV": "production",
         }
   }],
